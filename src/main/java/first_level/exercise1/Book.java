deb51b3f0e5680824e8fbdb8c942b122220b44b4
isalvama <inesalvans@gmail.com>
2026-02-13 20:19:55 +0100

    fix: remove unneeded getAuthor method from Book class

--- src/main/java/first_level/exercise1/Book.java
+++ src/main/java/first_level/exercise1/Book.java
@@ -5,16 +5,13 @@ import java.util.Objects;
 public class Book {
     private final String title;
     private final String author;
-public Book(String title, String author){
+
+    public Book(String title, String author){
     this.title = title;
     this.author = author;
 }
     public String getTitle() {
         return title;
-    }
-
-    public String getAuthor() {
-        return author;
     }
 
     @Override
