6d2387ce5f299b36af8f149c3aea231cc960b61d
isalvama <inesalvans@gmail.com>
2026-02-13 13:43:35 +0100

    feat: create CalculatorTest class with a first fail test

---
+++ src/test/java/third_level/CalculatorTest.java
@@ -0,0 +1,14 @@
+package third_level;
+import org.assertj.core.api.WithAssertions;
+import org.junit.jupiter.api.Test;
+
+
+class CalculatorTest implements WithAssertions {
+
+    @Test
+    void calculatorStartsWithTotalZero() {
+        Calculator calculator = new Calculator();
+        assertThat(calculator.getTotal()).isEqualTo(0);
+    }
+
+}
