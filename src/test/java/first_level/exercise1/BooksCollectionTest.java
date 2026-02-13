ca44eec7c02df58ac5e1d46ef18eafde43f746d4
isalvama <inesalvans@gmail.com>
2026-02-13 22:49:55 +0100

    fix: correction and refactor of test methods inBooksCollectionTest class

--- src/test/java/first_level/exercise1/BooksCollectionTest.java
+++ src/test/java/first_level/exercise1/BooksCollectionTest.java
@@ -1,32 +1,24 @@
 package first_level.exercise1;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class BooksCollectionTest {
-    //    Book book1 = new Book("Harry Potter", "J.K.Rowling");
-//    Book book2 = new Book("Pride And Projudice", "Jane Austen");
-//    Book book3 = new Book("To Kill a Mockingbird", "Harper Lee");
-//    Book book4 = new Book("One Hundred Years Of Solitude", "Gabriel García Márquez");
-//    Book book5 = new Book("Jane Eyre", "Charlotte Bronte");
-//
-//    BooksCollection bc = new BooksCollection("Classics");
 
-    // The collection should not be null after instancing the class
+    // TODO TEST ADD MUTLIPLE BOOKS
+
+    @DisplayName ("The collection should not be null after instancing the class")
     @Test
-    void notNullInstance() {
+    void instanceBooksCollection_bookArrayListIsNotNull() {
         BooksCollection bc = new BooksCollection("Classics");
-        Assertions.assertNotNull(bc);
+        Assertions.assertNotNull(bc.getBookArrayList());
     }
 
-    // The collection's size is correct after adding a few books.
+    @DisplayName ("The collection's size is correct after adding a few books")
     @Test
-    void validateArraySize() {
+    void addBook_validateBookArrayListSize() {
         BooksCollection bc = new BooksCollection("Classics");
         Book book1 = new Book("Harry Potter", "J.K.Rowling");
         Book book2 = new Book("Pride And Projudice", "Jane Austen");
@@ -37,10 +29,9 @@ class BooksCollectionTest {
         Assertions.assertEquals(3, bc.getBookArrayList().size());
     }
 
-    // The books are expected to be in the correct index after having added them in the collection
-// Adding a book in a specific position modifies the collection correctly.
+    @DisplayName ("The books are expected to be in the correct index after having added them in the collection")
     @Test
-    void validateIndexInArray() {
+    void addBook_IndexCorrectOrder() {
         BooksCollection bc = new BooksCollection("Classics");
         Book book1 = new Book("Harry Potter", "J.K.Rowling");
         Book book2 = new Book("Pride And Projudice", "Jane Austen");
@@ -59,9 +50,9 @@ class BooksCollectionTest {
         Assertions.assertEquals(book1, bc.getBookArrayList().get(1));
     }
 
-    //    El mètode per obtenir un llibre per posició retorna el títol correcte.
+    @DisplayName ("Adding a book in a specific position modifies the collection correctly.")
     @Test
-    void getTitleByPosition() {
+    void addBookInPosition_modificationIndexOfBooksInArray() {
         BooksCollection bc = new BooksCollection("Classics");
         Book book1 = new Book("Harry Potter", "J.K.Rowling");
         Book book2 = new Book("Pride And Projudice", "Jane Austen");
@@ -71,15 +62,39 @@ class BooksCollectionTest {
         bc.addBook(book1);
         bc.addBook(book2);
         bc.addBook(book3);
-        bc.addBook(book4);
-        bc.addBook(book5);
-        Assertions.assertEquals("Pride And Projudice", bc.getTitleFromPosition(1));
-        Assertions.assertEquals("Harry Potter", bc.getTitleFromPosition(0));
-        Assertions.assertEquals("One Hundred Years Of Solitude", bc.getTitleFromPosition(3));
+        bc.addBookInPosition(1, book4);
+        Assertions.assertEquals(book4, bc.getBookArrayList().get(1));
+        bc.addBookInPosition(0, book5);
+        Assertions.assertEquals(book5, bc.getBookArrayList().get(0));
+        Assertions.assertEquals(book1, bc.getBookArrayList().get(1));
+        Assertions.assertEquals(book2, bc.getBookArrayList().get(3));
 
     }
 
-    //    Eliminar un llibre pel títol redueix la mida de la col·lecció.
+    @DisplayName ("The method getTitleFromPosition returns the correct title")
+    @Test
+    void getTitleByPosition_returnsTitleOfBookInPosition() {
+        BooksCollection bc = new BooksCollection("Classics");
+        Book book1 = new Book("Harry Potter", "J.K.Rowling");
+        Book book2 = new Book("Pride And Prejudice", "Jane Austen");
+        Book book3 = new Book("To Kill a Mockingbird", "Harper Lee");
+        Book book4 = new Book("One Hundred Years Of Solitude", "Gabriel García Márquez");
+        Book book5 = new Book("Jane Eyre", "Charlotte Bronte");
+        bc.addBook(book1);
+        bc.addBook(book2);
+        bc.addBook(book3);
+        bc.addBook(book4);
+        bc.addBook(book5);
+        Assertions.assertEquals("Pride And Prejudice", bc.getTitleFromPosition(1));
+        Assertions.assertNotEquals("Pride And Prejudice", bc.getTitleFromPosition(0));
+        Assertions.assertEquals("Harry Potter", bc.getTitleFromPosition(0));
+        Assertions.assertNotEquals("Harry Potter", bc.getTitleFromPosition(3));
+        Assertions.assertEquals("One Hundred Years Of Solitude", bc.getTitleFromPosition(3));
+        Assertions.assertNotEquals("One Hundred Years Of Solitude", bc.getTitleFromPosition(1));
+
+    }
+
+    @DisplayName("removeBookByTitle reduces the collection's size")
     @Test
     void removeBookByTitle() {
         BooksCollection bc = new BooksCollection("Classics");
@@ -99,9 +114,9 @@ class BooksCollectionTest {
 
     }
 
-    //    La llista ordenada retorna els llibres en ordre alfabètic (sense modificar la col·lecció original).
+    @DisplayName("The ordered list returns the books in alphabetic order without modifying the original collection")
     @Test
-    void getOrderedCollection() {
+    void getOrderedCollection_returnsListInAlphabeticOrder() {
         BooksCollection bc = new BooksCollection("Classics");
         Book book1 = new Book("Harry Potter", "J.K.Rowling");
         Book book2 = new Book("Pride And Prejudice", "Jane Austen");
@@ -113,12 +128,18 @@ class BooksCollectionTest {
         bc.addBook(book3);
         bc.addBook(book4);
         bc.addBook(book5);
+        Assertions.assertEquals("Harry Potter", bc.getOrderedCollection().get(0).getTitle());
         Assertions.assertEquals("Jane Eyre", bc.getOrderedCollection().get(1).getTitle());
+        Assertions.assertEquals("One Hundred Years Of Solitude", bc.getOrderedCollection().get(2).getTitle());
+        Assertions.assertEquals("Pride And Prejudice", bc.getOrderedCollection().get(3).getTitle());
+        Assertions.assertEquals("To Kill a Mockingbird", bc.getOrderedCollection().get(4).getTitle());
+
+
     }
 
-    // No s’han de permetre llibres amb títols duplicats.
+    @DisplayName ("Books with duplicated titles cannot be added in the bookArrayList")
     @Test
-    void addBook() {
+    void addBook_DuplicatedTitleBookIsNotAdded() {
         BooksCollection bc = new BooksCollection("Classics");
         Book book1 = new Book("Harry Potter", "J.K.Rowling");
         Book book2 = new Book("Pride And Prejudice", "Jane Austen");
