6de793205eb9320afdc7abc29774d07fb951adc1
isalvama <inesalvans@gmail.com>
2026-02-13 14:01:50 +0100

    feat: refactor Calculator class and getTotal method adding total attribute and constructor.

--- src/main/java/third_level/Calculator.java
+++ src/main/java/third_level/Calculator.java
@@ -1,5 +1,14 @@
 package third_level;
 
 public class Calculator {
+    private int total;
+
+    public Calculator (){
+        this.total = 0;
+    }
+
+    public double getTotal(){
+        return total;
+    }
 
 }
