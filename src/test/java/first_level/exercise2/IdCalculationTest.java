bd851188b4476b30ae7b61e91193cc8ef11c6d59
isalvama <inesalvans@gmail.com>
2026-02-14 17:39:05 +0100

    test: refactor and add more parameters to calculateLetters_ShouldGenerateTheExpectedLetter method, create calculateLetters_ThrowsIllegalArgumentExceptionWhenEnteringNumOfMOreThan8Digits and calculateLetters_ThrowsIllegalArgumentExceptionWhenEnteringNegativeNum methods, add @DisplayName annotation to each of them with description

--- src/test/java/first_level/exercise2/IdCalculationTest.java
+++ src/test/java/first_level/exercise2/IdCalculationTest.java
@@ -1,39 +1,43 @@
 package first_level.exercise2;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static first_level.exercise2.IdCalculation.calculateIdLetter;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-/*
-Aprendràs a:
-
-Escriure una funció testejable basada en una fórmula matemàtica.
-Aplicar @ParameterizedTest per comprovar múltiples casos de forma elegant.
-Analitzar els resultats i detectar possibles errors en cas que alguna dada no coincideixi amb l’esperat.
-Enunciat
-Crea una classe anomenada CalculoDni que contingui un mètode públic per calcular la lletra corresponent d’un DNI, donat el número (sense lletra).
-
-Crea una classe de test amb JUnit 5 que parametritzi una sèrie de proves per validar el comportament correcte del càlcul.
-
-El test ha de comprovar com a mínim 10 casos de números de DNI diferents amb la seva lletra corresponent, validant que el resultat del mètode coincideix amb el valor esperat.
-
-També s'ha de validar valors invàlids, com números negatius o massa grans, per comprovar que el mètode gestiona correctament aquestes situacions (per exemple, llençant una excepció).
- */
 
 class IdCalculationTest {
 
+    @DisplayName ("calculateLetters method returns the correct char")
     @ParameterizedTest
-    @CsvSource({"46218815,q", "48095145,y", "35075664,c"})
+    @CsvSource({"46218815,q", "48095145,y", "35075664,c", "65004204,v", "39733877,c", "72966599,l", "73332793,f", "27586404,c", "04384163,h", "07167479,n", "07167479,n", "02331334,p", "03703023,t"})
+    void calculateLetters_ShouldGenerateTheExpectedLetter(String num, char expectedC){
+        Assertions.assertEquals(expectedC, calculateIdLetter(num));
+    }
 
-    void calculateLetters_ShouldGenerateTheExpectedLetter(long num, char expectedC){
-        IdCalculation id = new IdCalculation(num);
-        Assertions.assertEquals(expectedC, id.calculateIdLetter());
+    @DisplayName ("calculateLetters method throws IllegalArgumentException exception when passing numbers of more than 8 letters.")
+    @ParameterizedTest
+    @CsvSource({"12345678901"})
+    void calculateLetters_ThrowsIllegalArgumentExceptionWhenEnteringNumOfMOreThan8Digits(String num){
+       Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            calculateIdLetter(num);});
+        String actualMessage = exception.getMessage();
+        assertTrue(actualMessage.contains("idNumber can't have more than 9 digits"));
+
+    }
+
+    @DisplayName ("calculateLetters method throws IllegalArgumentException exception when passing negative numbers.")
+    @ParameterizedTest
+    @CsvSource({"-12345678", "-12345", "-98475"})
+    void calculateLetters_ThrowsIllegalArgumentExceptionWhenEnteringNegativeNum(String num){
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            calculateIdLetter(num);});
+        assertTrue(exception.getMessage().contains("idNumber can't be negative"));
+
     }
 
 }
