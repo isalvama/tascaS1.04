1240de1c2ca2cacf016356f48da3fbb318344725
isalvama <inesalvans@gmail.com>
2026-02-18 21:08:56 +0100

    fix: refactor calculateIdLetter() method by removing the switch structure and indexing a String with all the letters fix: change IllegalArgumentException message according to logic

--- src/main/java/first_level/exercise2/IdCalculation.java
+++ src/main/java/first_level/exercise2/IdCalculation.java
@@ -8,43 +8,19 @@ public class IdCalculation {
             throw new IllegalArgumentException("idNumber can't be negative");
         }
         if (idNumStringWithoutZeros.length() > 8) {
-            throw new IllegalArgumentException("idNumber can't have more than 9 digits");
+            throw new IllegalArgumentException("idNumber can't have more than 8 digits");
         }
         try{
             return Integer.parseInt(idNumStringWithoutZeros);
         } catch (NumberFormatException e) {
-            throw new NumberFormatException("The input number should only contain digits");
+            throw new NumberFormatException("idNumber should only contain numbers");
         }
     }
 
     public static char calculateIdLetter(String idNumber){
         int validatedIdNumber = convertToInt(idNumber);
         int remainder = validatedIdNumber % 23;
-        return switch (remainder){
-            case 0 -> 't';
-            case 1 -> 'r';
-            case 2 -> 'w';
-            case 3 -> 'a';
-            case 4 -> 'g';
-            case 5 -> 'm';
-            case 6 -> 'y';
-            case 7 -> 'f';
-            case 8 -> 'p';
-            case 9 -> 'd';
-            case 10 -> 'x';
-            case 11 -> 'b';
-            case 12 -> 'n';
-            case 13 -> 'j';
-            case 14 -> 'z';
-            case 15 -> 's';
-            case 16 -> 'q';
-            case 17 -> 'v';
-            case 18 -> 'h';
-            case 19 -> 'l';
-            case 20 -> 'c';
-            case 21 -> 'k';
-            case 22 -> 'e';
-            default -> throw new IllegalStateException("Unexpected value: " + remainder);
-        };
+        String letters = "TRWAGMYFPDXBNJZSQVHLCKE";
+        return letters.charAt(remainder);
     }
 }
